0a9326d3304f213f5669a4abd35d03b61febc009
Tziri <tzirizohn1@gmail.com>
2019-05-29 00:20:23 -0400

    Homework 5-27-19

---
+++ Homework 5-27-19/ClientApp/src/Buttons.js
@@ -0,0 +1,44 @@
+﻿import React from 'react';
+
+class Buttons extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            number1: 0,
+            number2: 1,
+            color: ""
+        };
+    }      
+
+    OnClick = () => {
+        this.setState({ number1: this.state.number1 + this.state.number2 });
+    }
+
+    OnClick2 = () => {      
+        this.setState({ number2: 1 + this.state.number2 });
+    }
+
+    OnClick3 = () => {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+            this.setState({ color})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 style={{ color: this.state.color }}>{this.state.number1}</h1>
+                <button onClick={this.OnClick} className="btn btn-primary">Add #</button>
+                <h1>{this.state.number2}</h1>
+                <button onClick={this.OnClick2} className="btn btn-success">amnt to add to #1</button>   
+                <button onClick={this.OnClick3} className="btn btn-danger">Change color</button>
+            </div>
+        );
+    }
+}
+
+
+export default Buttons;
--- Homework 5-27-19/ClientApp/src/index.js
+++ Homework 5-27-19/ClientApp/src/index.js
@@ -1,19 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import Buttons from './Buttons';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
+render(<Buttons/>, document.getElementById('root'));
